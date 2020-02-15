56a16a838c53a389a99bd391d2b4b0b88016ccfa
yaron kinar <yaronkinar@gmail.com>
2020-02-15 22:24:36 +0200

    loader

--- src/Components/Upload.js
+++ src/Components/Upload.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+
+
+import Spinner from "react-bootstrap/Spinner";
 import '../upload.css';
 const deepai = require("deepai")
 class ImageUpload extends React.Component {
     constructor(props) {
         super(props);
         this.image = React.createRef();
-        this.state = {file: '',imagePreviewUrl: '',imageResult:''};
+        this.state = {file: '',imagePreviewUrl: '',imageResult:'',loading:false};
     }
 
     _handleSubmit= (e) => {
@@ -19,13 +21,18 @@ class ImageUpload extends React.Component {
         deepai.setApiKey('292a51da-6e6d-41da-9eaa-e8dd12ea0b04');
         const node = this.image.current;
         (async ()=> {
+
+            this.setState({
+                loading:true
+            })
             var resp = await deepai.callStandardApi("colorizer", {
                 image: node,
             });
             console.log(resp)
 
             this.setState({
-                imageResult:resp.output_url
+                imageResult:resp.output_url,
+                loading:false
             })
          //   $("#container").attr('src', resp.output_url)
 
@@ -53,6 +60,7 @@ class ImageUpload extends React.Component {
     render() {
         let {imagePreviewUrl} = this.state;
         let {imageResult} = this.state;
+        let {loading} = this.state;
         let $imagePreview = null;
         let $imageResultPreview = null;
 
@@ -64,6 +72,10 @@ class ImageUpload extends React.Component {
         if(imageResult){
             $imageResultPreview = (<img src={imageResult} />)
         }
+        if(loading){
+            $imageResultPreview = (<Spinner animation="border" />)
+        }
+
 
         return (
             <div className="previewComponent">
@@ -72,6 +84,7 @@ class ImageUpload extends React.Component {
                            type="file"
                            ref={this.image}
                            onChange={(e)=>this._handleImageChange(e)} />
+                    <Button as="input" type="submit" value="Color Image" onClick={(e)=>this._handleSubmit(e)}/>
                     <button className="submitButton"
                             type="submit"
                             onClick={(e)=>this._handleSubmit(e)}>Color Image</button>
